f996f1fa36cc81a1421fa6496d51f174c1158def
abhinavchaudhary10 <abhinavchaudhary10@gmail.com>
2022-07-18 09:25:34 +0530

    First version of application moved into GitHub

--- hotel/settings.py
+++ hotel/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3tv)!eean8+22)5j-abjw0($4ignozwsipp+h%(7uijxwfss+q'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3tv)!eean8+22)5j-abjw0($4ignozwsipp+h%(7uijxwfss+q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- hotel/urls.py
+++ hotel/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('booking/', include('booking.urls')),
+    path('booking', include('booking.urls')),
     # path('dashboard/', include(dashboard_urls)),
 ]
